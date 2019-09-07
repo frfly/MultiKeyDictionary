b4652998f1427946cf2dea8a6202bc49a9d285fd
Андрей Симонов <a.simonov@bars-open.ru>
2019-09-08 00:17:14 +0700

    Create solution

---
+++ MultiKeyDictionary/MultiKeyDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiKeyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
